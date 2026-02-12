7398db201c5313211f6b8d8941ad14cc39afa039
kevinmartinez43 <km9767721@gmail.com>
2026-02-12 15:56:32 -0800

    agrega los cambios de sesion_02.py

--- sesion_01.py
+++ sesion_01.py
@@ -1,20 +1,20 @@
-# numeros
+# Numeros
 print(int(7))
-print(float(7.7))
+print (float(7.7))
 print(type(7))
-print(type(7.7))
-print(int(1+2))
-print(int(10*2))
-print(int(1 + 4 - 2))
-print(float(1 + 2.0))
+print (type(7.7))
+print(int(1 + 2))
+print (int (10*2))
+print(int(1 + 4 -2))
+print(int(1 + 2.0))
 
-# operadores matematicos
+# Operadores matematicos 
 # +
 # -
 # *
 # /
 # **
-# % modulo 
+# % Modulo
 
 print(int(2**3))
 print(int(4**8))
@@ -22,3 +22,46 @@ print(float(10%3))
 print(float(25%4))
 print(float(16%2))
 print(float(10 / 3))
+
+# Variables
+print("=====VARIABLES=====")
+x = 100 
+y = 1
+print(x+y)
+
+ventas = 1999991
+print("Nuestras ventas fueron",ventas)
+
+is_active = True
+print(is_active)
+
+game_over = False
+print(game_over)
+
+some_string = "Hola soy un string"
+print(some_string)
+
+print("=====Condicionales=====")
+edad = 18
+
+if (edad>=18):
+    print("Si puedes entrar al Bar")
+else:
+    print("No puedes entrar al Bar!!!")
+
+mi_numero = int (input("¿Cual es el numero que deseas verificar?"))
+print(f"El numero que deseas verificar es {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"El numero {mi_numero} es par!")
+else:
+    print(f"El numero {mi_numero} Es impar!!")
+
+def par_impar(numero):
+    if numero %2 == 0:
+        print(f"El numero {numero} es par!")
+    else:
+        print(f"El numero {numero} es impar!!")
+print("=====FUNCION PAR_IMPAR=====")
+mi_numero= int(input("¿Cual es el numero que deseas verificar?:"))
+print(f"El numero que desea verificar es {mi_numero}")
+print(par_impar(mi_numero))
---
+++ sesion_02.py
@@ -0,0 +1,18 @@
+# Tuplas 
+mi_tupla = (2, 4)
+print("Mi tupla: ",mi_tupla)
+
+ #Listas
+mi_lista = [1, 3.1416, "Javier",mi_tupla]
+print ("El primer elemento de mi lista", mi_lista[0])
+print ("El primer elemento de mi lista", mi_lista[3])
+print ("El primer elemento de mi lista", mi_lista[2])
+
+#Diccionarios
+mi_diccionario = {"mi_lista":mi_lista,
+                 "Nombre": "kevin",
+                 "Pi": 3.1416,
+                 "Tel":"661-2111391"}
+print("Llave para acceder a el diccionario mi_lista", mi_diccionario["mi_lista"])
+print("Llave para acceder a el diccionario Pi", mi_diccionario["Pi"])
+print("Llave para acceder a el dicionario Tel", mi_diccionario["Tel"])
